3ac54bbd3216be08865287629c6c037ed47004f1
Rudi Neuss <rudi.neuss@t-online.de>
2023-08-01 22:11:09 +0200

    test

--- FirstGutHubProject/ViewController.swift
+++ FirstGutHubProject/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var I: Int = 10
+    var i: Int = 10
+    var k: Int = 20
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
