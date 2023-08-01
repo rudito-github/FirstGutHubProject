d4555e0ab218b3a01cbf0cbf0155704f2a14284b
Rudi Neuss <rudi.neuss@t-online.de>
2023-08-01 22:10:04 +0200

    Initial Commit

---
+++ FirstGutHubProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstGutHubProject
+//
+//  Created by Peter Pim on 01.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var I: Int = 10
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
